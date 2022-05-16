6816152130095b8c23646ebbb1f2f1215fc78a77
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-16 16:04:51 -0500

    Add invalid input detector

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function computerPlay(){
      // This function is the one that chooses what the computer is going to throw
+     console.log("The computer chose: ")
     switch(Math.floor(Math.random()*3)+1){
         case 1:
             console.log("Rock");
@@ -30,26 +31,29 @@ function playerSelection(playerInput){
             return 3;
             break;
         default:
-            console.log("Invalid input");
             return 4;
             break;
     } 
 }
-var options = ["Rock", "Paper", "Scissors"]
+var options = ["Rock", "Paper", "Scissors"];
 function playRound(computerPlay,playerSelection){
     // this function is used to check who won 
+    if(playerSelection === 4) {
+        console.log("invalid input")
+        return 0;
+    }
     let result = computerPlay - playerSelection;
     
     if(result === 0){
-             return "It is a Tie, you both chose " + options[computerPlay-1];
+             return ("It is a Tie, you both chose " + options[computerPlay-1]);
     }
     if(result === -1 || result ===2){
-            return "You win " +options[playerSelection-1]+ " beats " +options[computerPlay-1];
+            return ("You win " +options[playerSelection-1]+ " beats " +options[computerPlay-1]);
     }
     if (result === 1 || result ===-2){
-            return "You lost " +options[playerSelection-1] + " looses to " + options[computerPlay-1];
+            return ("You lost " +options[playerSelection-1] + " looses to " + options[computerPlay-1]);
     }
 }
 
-let playerInput = prompt("This is rock paper scissors, write your choice: ", options[Math.floor(Math.random()*3)] )
-console.log(playRound(computerPlay,playerSelection(playerInput)))
+let playerInput = prompt("This is rock paper scissors, write your choice: ", options[Math.floor(Math.random()*3)] );
+console.log(playRound(computerPlay(),playerSelection(playerInput)));
