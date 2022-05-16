918b85fffad255db8d938795e0a80ef135e6c7aa
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-16 18:28:51 -0500

    Add comments to all project

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function computerPlay(){
             break;        
     }
 }
-function playerSelection(playerInput){
+function playerSelection(playerInput){// function to convert the input to numbers (used to check the result of the winner)
     playerInput= playerInput.trim();
     playerInput= playerInput.toLowerCase();
     switch(playerInput){
@@ -38,9 +38,9 @@ function playerSelection(playerInput){
 var options = ["Rock", "Paper", "Scissors"];
 function playRound(computerPlay,playerSelection){
     // this function is used to check who won 
-    let result = computerPlay - playerSelection;
+    let result = computerPlay - playerSelection; 
     
-    if(result === 0){
+    if(result === 0){ 
             console.log("It is a Tie, you both chose " + options[computerPlay-1]);
             return 0
     }
@@ -53,23 +53,32 @@ function playRound(computerPlay,playerSelection){
             return false
     }
 }
+
+//Game start 
 let userWins=0;
 let computerWins =0;
 for(let i=0; i<5;i++){
-    console.log("round "+ (i+1))
+    console.log("round "+ (i+1)) 
+
     playerInput= prompt("write in your choice: ")
-    let loopVerifier = playerSelection(playerInput)
+    let loopVerifier = playerSelection(playerInput)     //ask for input and verify that it is not a invalid input
     while(loopVerifier===4){
         playerInput = prompt("Write a valid input")
         loopVerifier=playerSelection(playerInput)
     }
-    winner = playRound(computerPlay(),playerSelection(playerInput))
+
+
+    winner = playRound(computerPlay(),playerSelection(playerInput)) //after every playRound , add the points to the computer or user depending on who won
     if(winner===true){ userWins++;}
     else if(winner===false){computerWins++}
     else{console.log("Tie")}
+
+
     console.log("")
 
 }
+
+// print the results
 if(computerWins>userWins){ console.log("Computer wins the game")}
 else if(userWins>computerWins){console.log("You win the game")}
 else{console.log("The game is a tie\n")}
