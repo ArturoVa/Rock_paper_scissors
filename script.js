177c0c4eba7c2fb2a0924028c33aedbfae4bdd45
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-05-20 18:25:11 -0500

    Made changes to game function buttons
    Removed the computer selection and replaced it with computer choice, set the gameRock(), gamePaper(), gameScissors(), which are the game functions for each button

--- script.js
+++ script.js
@@ -1,66 +1,46 @@
-var options = ["Rock", "Paper", "Scissors"];
-function computerPlay(){
-     // This function is the one that chooses what the computer is going to throw
-     console.log("The computer chose: ")
-    switch(Math.floor(Math.random()*3)+1){
-        case 1:
-            console.log("Rock");
-            return 1;
-            break;
-            
-        case 2:
-            console.log("Paper");   
-            return 2;
-            break;
-        case 3:
-            console.log("Scissors");
-            return 3;
-            break;        
-    }
-}
-function playerSelection(playerInput){// function to convert the input to numbers (used to check the result of the winner)
-    playerInput= playerInput.trim();
-    playerInput= playerInput.toLowerCase();
-    switch(playerInput){
-        case "rock":
-            return 1
-            break;
-        case "paper":
-            return 2;
-            break;
-        case "scissors":
-            return 3;
-            break;
-        default:
-            return 4;
-            break;
-    } 
-}
-
 function playRound(computerPlay,playerSelection){
     // this function is used to check who won 
+    var options = ["Rock", "Paper", "Scissors"];
     let result = computerPlay - playerSelection; 
     
     if(result === 0){ 
-            console.log("It is a Tie, you both chose " + options[computerPlay-1]);
+            console.log("It is a Tie, you both chose " + options[computerPlay]);
             return 0
     }
     if(result === -1 || result ===2){
-            console.log("You win " +options[playerSelection-1]+ " beats " +options[computerPlay-1]);
+            console.log("You win " +options[playerSelection]+ " beats " +options[computerPlay]);
             return true
     }
     if (result === 1 || result ===-2){
-            console.log("You lost " +options[playerSelection-1] + " looses to " + options[computerPlay-1]);
+            console.log("You lost " +options[playerSelection] + " looses to " + options[computerPlay]);
             return false
     }
 }
+function gameRock() {
+    let computerChoice = Math.floor(Math.random()*3);
+    let playerSelection = 0
+    console.log(playRound(computerChoice,playerSelection));
+}
+function gamePaper() {
+    let computerChoice = Math.floor(Math.random()*3);
+    let playerSelection = 1
+    console.log(playRound(computerChoice,playerSelection));
+}
+function gameScissors(){
+    let computerChoice = Math.floor(Math.random()*3);
+    let playerSelection = 2
+    console.log(playRound(computerChoice,playerSelection));
+}
 
-const btn_rock = document.querySelector("#Rock")
-const btn_paper = document.querySelector("#Paper")
-const btn_scissors = document.querySelector("#Scissors")
+const btn_rock = document.querySelector("#Rock");
+const btn_paper = document.querySelector("#Paper");
+const btn_scissors = document.querySelector("#Scissors");
+btn_rock.addEventListener('click',gameRock);
+btn_paper.addEventListener('click',gamePaper);
+btn_scissors.addEventListener('click',gameScissors);
 
 //Game start 
-btn_paper.addEventListener('click',)
+
 
 
 
